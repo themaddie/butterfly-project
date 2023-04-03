85f22f17ffbdd18d8d848eed1e331dfdcee76e93
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-04-03 23:52:38 +0330

    Develop Plot & Bazinga components

--- butterfly/src/Components/Bazinga.js
+++ butterfly/src/Components/Bazinga.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// Styles
+import Style from '../Styles/Bazinga.module.css';
+// Images
+import bazingaIndex from '../Images/bazingaIndex.JPG';
 
 const Bazinga = () => {
+    const [bazingaTitle, setBazingaTitle] = useState("عنوان خیلی الکی");
+    const [bazingaDes, setBazingaDes] = useState("متن کاملا فرضی");
     return (
-        <div>
-            اینجا بازینگاست
+        <div className={Style.container}>
+            <img className={Style.bazingaIndex} src={bazingaIndex} />
+            <div className={Style.bazingaContainer}>
+                <h1 className={Style.bazingaTitle}>{bazingaTitle}</h1>
+                <p className={Style.bazingaDes}>{bazingaDes}</p>
+                <Link to="/bazinga" className={Style.bazingaBtn}>بفشار!</Link>
+            </div>
         </div>
     );
 };
--- butterfly/src/Components/Plot.js
+++ butterfly/src/Components/Plot.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// Styles
+import Style from '../Styles/Plot.module.css';
+// Images
+import plotIndex from '../Images/plotIndex.png';
 
 const Plot = () => {
+    const [plotTitle, setPlotTitle] = useState("عنوان خیلی الکی");
+    const [plotDes, setPlotDes] = useState("متن کاملا فرضی");
     return (
-        <div>
-           اینجا پلات ست 
+        <div className={Style.container}>
+            <div className={Style.plotContainer}>
+                <h1 className={Style.plotTitle}>{plotTitle}</h1>
+                <p className={Style.plotDes}>{plotDes}</p>
+            <Link to="/plot" className={Style.plotBtn}>بفشار!</Link>
+            </div>
+            <img className={Style.plotIndex} src={plotIndex} />
         </div>
     );
 };
