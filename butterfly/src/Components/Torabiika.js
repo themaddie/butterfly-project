d14a4d50d8aa4987b63985530a154172377688a6
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-04-03 21:13:56 +0330

    Develop Torabiika component

--- butterfly/src/Components/Torabiika.js
+++ butterfly/src/Components/Torabiika.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// Styles
+import Style from '../Styles/Torabiika.module.css';
 
-const torabiika = () => {
+const Torabiika = () => {
+    const [torabiikaTitle, setTorabiikaTitle] = useState("عنوان خیلی الکی");
+    const [torabiikaDes, setTorabiikaDes] = useState("متن کاملا فرضی")
     return (
-        <div>
-            اینجا ترابیکاست
+        <div className={Style.container}>
+            <h1 className={Style.torabiikaTitle}>{torabiikaTitle}</h1>
+            <p className={Style.torabiikaDes}>{torabiikaDes}</p>
+            <Link to="/about" className={Style.torabiikaBtn}>بفشار!</Link>
         </div>
     );
 };
 
-export default torabiika;
+export default Torabiika;
