daf4c2e040cc4e1e9a9e53d901652a3ce72cccab
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-04-21 13:37:41 +0330

    Set delete icon layout for todo

--- butterfly/src/Components/Plot/TodayList.js
+++ butterfly/src/Components/Plot/TodayList.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 // Styles
 import Style from '../../Styles/Plot/TodayList.module.css';
 // Images
-import editIcon from '../../Images/Plot/edit.png';
+import deleteIcon from '../../Images/Plot/delete.png';
 
 const TodayList = (props) => {
+
+    const [showDeleteIcon, setShowDeleteIcon] = useState(false);
 
     return (
         <div className={Style.container}>
             <div className={Style.todos}>
                 <div className={Style.todo}>
-                    <div style={{ display: 'flex' }}>
-                        <img className={Style.edit} alt='edit' src={editIcon} />
+                    <div className={Style.box} onMouseEnter={() => setShowDeleteIcon(true)} onMouseLeave={() => setShowDeleteIcon(false)}>
+                        {showDeleteIcon ? (
+                            <img className={Style.deleteTodo} alt='delete' src={deleteIcon} />
+                        ) : ''}
                         <span className={Style.text}>{props.text}</span>
                     </div>
                     <div className={Style.checkbox}>{props.done}</div>
