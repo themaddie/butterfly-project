b2eb0b55da43096a403df5bbf2cc2740b20b9aff
Maddie <99765525+themaddie@users.noreply.github.com>
2024-09-09 02:08:49 +0330

    Update Torabiika.js

--- butterfly/src/Components/Torabiika.js
+++ butterfly/src/Components/Torabiika.js
@@ -7,7 +7,7 @@ import Style from '../Styles/Torabiika.module.css';
 
 const Torabiika = () => {
     const [torabiikaTitle, setTorabiikaTitle] = useState("ترابیکا کیه؟");
-    const [torabiikaDes, setTorabiikaDes] = useState("ترابیکا، یه دختر معمولی و کمی تا اندکی تنهاست. اون داره نهایت تلاشش رو برای پیدا کردن مسیرش توی این دنیا میکنه اما تا الان موفق نبوده و احتمالا موفق هم نمیشه...");
+    const [torabiikaDes, setTorabiikaDes] = useState("...ترابیکا، یه دختر معمولیه. اون داره نهایت تلاشش رو برای پیدا کردن مسیرش توی این دنیا میکنه...");
     return (
         <motion.div whileHover={{ scale: 1.1 }} className={Style.container}>
             <h1 className={Style.torabiikaTitle}>{torabiikaTitle}</h1>
